e28df59b454a3835ebd4bcf37af7396ec1f942f2
OctoOcto <66621960+OctoOcto@users.noreply.github.com>
2020-08-09 14:24:44 +0200

    UserInfo fixed

--- routes/User.js
+++ routes/User.js
@@ -4,6 +4,8 @@ const app = express();
 const formidableMiddleware = require("express-formidable");
 const router = express.Router();
 
+const UserIngo = require("../models/UserInfo");
+
 //Import crypto
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
